6dae124972dc59a6d32f6edcc73140a0adb6781f
Lorenzo Manunza <99339834+Manunz15@users.noreply.github.com>
2024-04-16 09:37:30 +0200

    Double layer graphene conductivity

--- graphene_conductivity.py
+++ graphene_conductivity.py
@@ -1,3 +1,5 @@
+# Lorenzo Manunza, Università degli Studi di Cagliari, April 2024
+
 from create.hex import HexLattice
 from create.second_layer import add_second_layer
 from create.box import create_box
@@ -8,9 +10,10 @@ from utils.modify_file import modify_file
 from utils.path import Path
 from utils.materials import*
 
+import numpy as np
+
 lattice = 'graphene'
-dir_name = 'conductivity_double_layer'
-angle = 1.1
+dir_name = 'conductivity'
 files = ['/'.join(['lammps', lattice, 'CH.airebo']),
          '/'.join(['lammps', lattice, 'in.CONDUCTIVITY'])]
 
@@ -19,18 +22,17 @@ step = lattices[lattice]['step']
 atoms = lattices[lattice]['atoms']
 
 DF = HexLattice().create(step = step, dim = (121, 105), atom_types = 1)
-DF = add_second_layer(DF, angle = angle, trasl = [0, 0, 3.3])
 box = create_box(DF, delta = [step, step, 2000])
-# PlotCrystal().plot_2d([DF])
+PlotCrystal().plot_2d([DF])
 
-changes = {'x_1i': box[0][0],
-           'x_1f': (box[0][1] - box[0][0]) / 2,
-           'x_2i': (box[0][1] - box[0][0]) / 2 + 0.1,
-           'x_2f': box[0][1]}
+changes = {'x_1i': round(box[0][0], 3),
+        'x_1f': round((box[0][1] - box[0][0]) / 2, 3),
+        'x_2i': round((box[0][1] - box[0][0]) / 2 + 0.1, 3),
+        'x_2f': round(box[0][1], 3)}
 
 # save
 path = Path(path = [lattice, dir_name])
 path.copy(files = files)
 modify_file(filename = '/'.join([path.path,'in.CONDUCTIVITY']), changes = changes)
-WriteAtoms().write(DF = DF, atoms = atoms, filename = f'../simulations/graphene/{dir_name}/atoms.dat', 
+WriteAtoms().write(DF = DF, atoms = atoms, filename = f'../simulations/{lattice}/{dir_name}/atoms.dat', 
             title = f'Lorenzo Manunza {today()}', box = box)
