7cc20ce41da823d9cc8216a09efbd594c1341b10
Lorenzo Manunza <99339834+Manunz15@users.noreply.github.com>
2024-05-07 14:38:11 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+# Lorenzo Manunza, Università degli Studi di Cagliari, May 2024
+
+from lattice import HexLattice
+
+# create and plot
+graphene = HexLattice('graphene', dim = (1, 1), angle = 0.9)
+graphene.plot()
+graphene.remove_overlapping_atoms()
+graphene.plot()
