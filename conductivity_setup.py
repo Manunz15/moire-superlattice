216f6cefe0daa1bf918c180efe71827af3ec10fb
Lorenzo Manunza <99339834+Manunz15@users.noreply.github.com>
2024-05-13 16:17:18 +0200

    Update conductivity_setup.py

--- conductivity_setup.py
+++ conductivity_setup.py
@@ -3,7 +3,7 @@ from lattice import HexLattice, create_all, Lattice
 # twisted double layer
 DIMS = [(width, 1) for width in range(1, 7)]
 ANGLES = [0.6, 0.8]
-create_all(lattice = 'graphene', DIMS = DIMS, ANGLES = ANGLES, dir_name = 'lv_conductivity/angles', lammps = 'in.LV_CONDUCTIVITY')
+create_all(lattice = 'graphene', DIMS = DIMS, ANGLES = ANGLES, dir_name = 'lv_conductivity/more_angles', lammps = 'in.LV_CONDUCTIVITY')
 
 # onelayer and bilayer
 # height = 100
