f479d69b219ae3bdbc6f4220c57ea00188972697
Lorenzo Manunza <99339834+Manunz15@users.noreply.github.com>
2024-06-11 17:20:30 +0200

    Print box after box/relax

--- conductivity_analysis.py
+++ conductivity_analysis.py
@@ -2,6 +2,9 @@
 
 from analysis import Lampin, ExtrConductivity, MoireConductivity, Section
 
+lp = Lampin('../../dim_1x1', 'graphene')
+lp.plot()
+lp.plot_temp()
 # sec = Section('../data/graphene/almost_final/test_section_1.50', 'graphene')
 # sec.plot()
 
