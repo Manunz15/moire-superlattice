78324e8369cb84f7316606582c85eedc0ee17e53
Lorenzo Manunza <99339834+Manunz15@users.noreply.github.com>
2024-12-10 11:34:08 +0100

    EVEN MORE DATA

--- even_more_data.py
+++ even_more_data.py
@@ -1,7 +1,7 @@
 from lattice import HexLattice, create_all, Lattice
 
 lattice = 'LJ-graphene'
-lammps = 'in.LV_CONDUCTIVITY_II'
+lammps = 'in.LV_CONDUCTIVITY_III'
 
 # twisted double layer
 DIMS = [(width, 1) for width in range(1, 7 + 1)]
--- phonons.py
+++ phonons.py
@@ -3,5 +3,8 @@
 from lattice import HexLattice
 
 # create and plot
-graphene = HexLattice('graphene', dim = (1, 1), angle = 1.1)
-graphene.write_alm('../phonons/graphene/first_try/graphene_alm.in')
+for angle in [0.91, 1.02, 1.05, 1.08, 1.12, 1.2, 1.35, 1.54]:
+    graphene = HexLattice('LJ-graphene', dim = (1, 1), angle = angle)
+    graphene.remove_overlapping_atoms()
+    graphene.write_lammps(f'../data/phonons/TBG {angle}.dat')
+    # print(graphene)
